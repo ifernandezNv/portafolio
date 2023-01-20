66d7cb4f6f0ca8b4efeae670830c9133c440d79e
ifernandeznv <ifernandeznv@gmail.com>
2023-01-20 17:20:12 -0600

    Creación del theme de estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js}"],
   theme: {
-    extend: {},
+    colors: {
+      "bgNegro": '#0B111E',
+      "span": '#1692AA',
+      "bgAzul": '#047E96'
+    },
+    fontFamily: {
+      "lato": ['Lato', 'sans-serif'],
+      "poppins": ['Poppins', 'sans-serif']
+    },
+    extend: {
+      borderRadius: {
+        DEFAULT: "10px 10px 10px 10px",
+        "top": "10px 10px 0 0"
+      },
+      boxShadow: {
+        DEFAULT: "0px 1px 60px rgba(180, 169, 227, 0.2)"
+      }
+    },
   },
   plugins: [],
 }
