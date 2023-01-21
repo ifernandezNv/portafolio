177b40e8f417d90ee8134a2bb537bedf5787ead5
ifernandeznv <ifernandeznv@gmail.com>
2023-01-20 21:13:53 -0600

    Ajustes de estilos

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={`min-h-screen bg-azul-oscuro relative`}>
+      <header className={`h-5/6 bg-azul-oscuro relative`}>
             <Image 
               src={'/assets/bgHeader.png'}
               alt='Imagen bg-header'
@@ -24,14 +24,14 @@ export default function Home() {
               width={1200}
               quality={100}
               fit={true}
-              className='min-h-screen w-full relative blur '
+              className='w-full relative blur h-5/6'
             />
-            <div className='flex flex-col items-center  justify-center absolute top-0 z-50 w-full min-h-screen' >
+            <div className='flex flex-col items-center  justify-center absolute -top-28 z-50 w-full min-h-screen' >
               <h1 className='text-white text-center text-4xl font-bold tracking-wider'>Hola, Soy Isaac</h1>
-              <h2 className='text-2xl font-bold mt-9 text-white'>Desarrollador <span className='text-span'>Front-End</span></h2>
-              <p className='text-white text-center text-lg font-medium'>Conoce un poco más acerca de mí</p>
+              <h2 className='text-2xl font-bold mt-16 text-white'>Desarrollador <span className='text-span'>Front-End</span></h2>
+              <p className='text-white text-center text-lg font-medium mt-10'>Conoce un poco más acerca de mí</p>
               <p className='text-white text-center text-lg font-medium'>y mira mis proyectos más destacables</p>
-              <div className='flex flex-col gap-4 mt-9'>
+              <div className='flex justify-center gap-4 mt-9'>
                 <div className='flex gap-3'>
                   <Link href='https://github.com/ifernandezNv' alt='Github profile' className='text-white text-sm font-bold'>Github</Link>
                 </div>
@@ -39,6 +39,10 @@ export default function Home() {
                   <Link href='https://github.com/ifernandezNv' alt='Github profile' className='text-white text-sm font-bold'>LinkedIn</Link>
                 </div>
               </div>
+              <div className='flex gap-12 mt-14'>
+                  <Link href='#proyectos' className='text-center text-white shadow font-semibold px-10 py-4 bg-bgAzul rounded'>Ver Proyectos</Link>
+                  <a href="" download className='text-black px-10 py-4 font-semibold rounded bg-white'>Descargar CV</a>
+                </div>
             </div>
 
       </header>
