14cccc55be08c4f894f09d544f569f6fa8ddfb53
ifernandeznv <ifernandeznv@gmail.com>
2023-01-23 14:37:49 -0600

    Ajustes Generales de estilos

--- src/components/Tecnologia.js
+++ src/components/Tecnologia.js
@@ -4,7 +4,7 @@ function Tecnologia({tecnologia}) {
     const {nombre, icono} = tecnologia;
   return (
     <div className='flex gap-1 md:gap-2 items-center'>
-        <p className='w-7 h-7 bg-azul-oscuro'></p>
+        <p className='w-6 h-6 lg:w-8 lg:h-8 bg-azul-oscuro'></p>
         <h4 className='text-movil-md md:text-label'>{nombre}</h4>
     </div>
   )
